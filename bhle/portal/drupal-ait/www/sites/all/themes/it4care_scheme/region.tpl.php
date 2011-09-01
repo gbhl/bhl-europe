ca93a5515c5eb4f83802ca8258a40e01a711b328
Jan Hladik <jan.hladik@it4care.cz>
2011-09-01 12:15:08 +0200

    uploaded new theme

---
+++ bhle/portal/drupal-ait/www/sites/all/themes/it4care_scheme/region.tpl.php
@@ -0,0 +1,8 @@
+<?php
+// $Id: region.tpl.php,v 1.1.2.2 2011/02/06 22:47:17 andregriffin Exp $
+?>
+<?php if (!empty($content)): ?>
+<div class="<?php print $classes; ?>">
+  <?php print $content; ?>
+</div>
+<?php endif; ?> <!-- /.region -->
