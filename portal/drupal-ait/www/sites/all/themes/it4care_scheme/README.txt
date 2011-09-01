cd99fd7b5d075933cda1849831f5e89bf4733444
Jan Hladik <jan.hladik@it4care.cz>
2011-09-01 12:42:42 +0200

    New template of http://bhl-portal-dev.nhm.ac.uk

---
+++ portal/drupal-ait/www/sites/all/themes/it4care_scheme/README.txt
@@ -0,0 +1,41 @@
+// $Id: README.txt,v 1.4.2.3 2011/02/18 05:26:30 andregriffin Exp $
+
+bhleurope 3.x for Drupal 7
+Created by André Griffin
+http://drupal.org/project/bhleurope
+
+bhleurope is a blank canvas for theme developers. 
+Use bhleurope as a user friendly starting point to help facilitate your theme development. 
+Build site themes without feeling like you have to reinvent the wheel or remove unnecessary code every time.
+
+bhleurope 3 is intended to be used with the "Seven" administration theme included with Drupal 7.
+
+bhleurope is actively developed and supported on my own time. If you would like to say thanks, please consider donating via:
+https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=1532730
+
+Development sites:
+http://d7.andregriffin.com
+http://d6.andregriffin.com
+
+-------
+USAGE
+
+Install to sites/all/themes
+
+To fully convert bhleurope to your own custom theme, it is encouraged that you: 
+ - Copy the 'bhleurope' directory within sites/all/themes/ and rename it as 'yourthemename' 
+ - Change the filename and contents of the .info file accordingly
+ - Use find/replace in template.php to replace 'bhleurope' with 'yourthemename' 
+ - Upload and enable the theme
+ - Modify as needed
+
+To create a sub-theme based on bhleurope, see: http://drupal.org/node/225125
+
+-------
+SUPPORT
+
+If you have questions or problems, check the issue list before submitting a new issue: 
+http://drupal.org/project/issues/bhleurope
+
+For general support, please refer to:
+http://drupal.org/support
---
+++ portal/drupal-ait/www/sites/all/themes/it4care_scheme/maintenance-page.tpl.php
@@ -0,0 +1,61 @@
+<?php
+// $Id: maintenance-page.tpl.php,v 1.4.2.2 2011/02/18 05:26:30 andregriffin Exp $
+?><!DOCTYPE html>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+
+<head>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+
+<div id="wrapper" class="clearfix">
+
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+
+  <header id="header" role="banner" class="clearfix">
+	<?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+    <?php endif; ?>
+    <?php if ($site_name || $site_slogan): ?>
+      <div id="site-name-slogan">
+        <?php if ($site_name): ?>
+          <?php if ($title): ?>
+            <div id="site-name"><strong>
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </strong></div>
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
+          <?php endif; ?>
+        <?php endif; ?>
+        <?php if ($site_slogan): ?>
+          <div id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+    <?php print render($page['header']); ?>
+  </header> <!-- /#header -->
+
+  <section id="main" role="main" class="clearfix">
+    <?php print $messages; ?>
+    <a id="main-content"></a>
+    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+    <?php print $content; ?>
+  </section> <!-- /#main -->
+  
+</div> <!-- /#wrapper -->
+
+</body>
+</html>
