a839011f0c957b4dc7ebc4ef88908176a5bd1e28
Jan Hladik <jan.hladik@it4care.cz>
2011-10-14 00:13:12 +0200

    delete clone directory

--- bhle/access/booktoolbox/src/main/java/util/GlobalParameter.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class GlobalParameter {
-	public static String BASE_URL = "http://localhost:8080";
-	
-	public static String FEDORA = "fedora";
-	
-	public static String RESOURCE_INDEX = "risearch";
-}
