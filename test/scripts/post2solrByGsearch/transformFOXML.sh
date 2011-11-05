313f359a9149918049fc030660b1395679bf633b
a.kohlbecker <a.kohlbecker@bgbm.org>
2011-11-05 10:28:28 +0100

    transformFOXML.sh minor changes

--- test/scripts/post2solrByGsearch/transformFOXML.sh
+++ test/scripts/post2solrByGsearch/transformFOXML.sh
@@ -7,20 +7,20 @@ FGSEARCH_BASE=/home/andreas/workspaces/bhle/opt/fedoragsearch
 
 if [ "$1" == "" -o "$2" == "" ]; then
 	cat <<EOT
-Usage: $0 <foxml-file> <xsl-stylesheet>
+Usage: $0 <xsl-stylesheet> <foxml-file>
 
-e.g. $0 ~/dev/opt/test/foxml/test.xml ~/dev/opt/data-management/gsearch/foxmlToSolrDemo.xslt
+e.g. $0 ~/dev/opt/data-management/gsearch/foxmlToSolrDemo.xslt ~/dev/opt/test/foxml/test.xml
 
 
 EOT
 	exit 1
 fi
-if [ ! -e "$1" ]; then
-	echo "$1 is not a file"
+if [ ! -e "$2" ]; then
+	echo "$2 is not a file"
 	exit 2
 fi
 
-echo "transforming foxml file  $1"
+echo "transforming foxml file  $2"
 
 
 #for i in $(find $FGSEARCH_BASE/WEB-INF/lib -type f | grep "jar$")
@@ -28,9 +28,9 @@ echo "transforming foxml file  $1"
 OUT=/tmp/post2solr.out.xml
 
 
-IN=$1
+IN=$2
 echo "processing $IN .."
-$JAVA -classpath $FGSEARCH_BASE/WEB-INF/classes:$FGSEARCH_BASE/WEB-INF/lib/serializer.jar:$FGSEARCH_BASE/WEB-INF/lib/xml-apis.jar:$FGSEARCH_BASE/WEB-INF/lib/xercesImpl.jar:$FGSEARCH_BASE/WEB-INF/lib/xalan.jar:$FGSEARCH_BASE/WEB-INF/lib/log4j-1.2.15.jar org.apache.xalan.xslt.Process -in $IN -xsl $2 -out $OUT
+$JAVA -classpath $FGSEARCH_BASE/WEB-INF/classes:$FGSEARCH_BASE/WEB-INF/lib/serializer.jar:$FGSEARCH_BASE/WEB-INF/lib/xml-apis.jar:$FGSEARCH_BASE/WEB-INF/lib/xercesImpl.jar:$FGSEARCH_BASE/WEB-INF/lib/xalan.jar:$FGSEARCH_BASE/WEB-INF/lib/log4j-1.2.15.jar org.apache.xalan.xslt.Process -in $IN -xsl $1 -out $OUT
 
 /bin/less $OUT
 
