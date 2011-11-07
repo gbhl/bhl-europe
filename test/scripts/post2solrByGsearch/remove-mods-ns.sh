c2574b021753d957186b5c6ec0f815e77d784652
a.kohlbecker <a.kohlbecker@bgbm.org>
2011-11-07 22:15:16 +0100

    remove-mods-ns.sh: tweak to activate inactive fedora data objects (for development only)

--- test/scripts/post2solrByGsearch/remove-mods-ns.sh
+++ test/scripts/post2solrByGsearch/remove-mods-ns.sh
@@ -5,7 +5,7 @@ SEARCH_1="xmlns=\"http:\/\/www.loc.gov\/mods\/v3\""
 SEARCH_2="VALUE=\"Inactive\"\/>"
 REPLACE_2="VALUE=\"Active\"\/>"
 
-sed -e 's/'${SEARCH_1}'//' -e 's/'$REPLACE_2'/'$SEARCH_2'/' <$1
+sed -e 's/'${SEARCH_1}'//' -e 's/'$SEARCH_2'/'$REPLACE_2'/' <$1
 
 
 
