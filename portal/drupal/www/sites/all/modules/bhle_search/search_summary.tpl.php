e8718d58b5f8e98429f260eb8507348c3cd09feb
a.kohlbecker <a.kohlbecker@bgbm.org>
2011-11-10 17:05:36 +0100

    first implemetation of search_summry block - no real counts per term by now

--- portal/drupal/www/sites/all/modules/bhle_search/search_summary.tpl.php
+++ portal/drupal/www/sites/all/modules/bhle_search/search_summary.tpl.php
@@ -27,8 +27,9 @@
 */
 
 
-print ('<pre>' .var_dump($_SESSION['bhle_search']) .'</pre>');
-// print ( var_dump($number_of_results) . var_dump($matches_for) .'</pre>');
+//print ('<pre>' .var_dump($_SESSION['bhle_search']) .'</pre>');
+//print ('<pre>' . var_dump($number_of_results).'</pre>');
+//print ('<pre>' .  var_dump($matches_for) .'</pre>');
 
 ?>
 <div class="search-summary search-summary-<?php print $classes; ?>">
@@ -40,7 +41,7 @@ print ('<pre>' .var_dump($_SESSION['bhle_search']) .'</pre>');
 		<ul>
 		<?php foreach ($matches_for['search_conditions'] as $condition) : ?>
 			<li>
-				<?php if($condition['phrase']) :?><span class="exact">exact:</span><?php endif; ?>
+				<?php if($condition['exact_phrase'] == 1) :?><span class="exact">exact:</span><?php endif; ?>
 				<span class="terms"><?php print $condition['term']; ?></span>
 				<span class="count">(<?php print $condition['count']; ?>)</span>
 				<span class="field"><?php print $condition['field']; ?></span>
