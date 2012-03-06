3e926cadd367deecffe5e3ed4cb07d09f4c0dff0
andreas mehrrath <mehrrath@mindcatch.com>
2012-03-06 12:16:56 +0100

    update database !

--- pre-ingest/ingest/docs/database/upgrade.sql
+++ pre-ingest/ingest/docs/database/upgrade.sql
@@ -1,6 +1,8 @@
+-- -----------------------------------
+-- UPGRADE DATABASE TO CURRENT VERSION
+-- -----------------------------------
+ALTER TABLE `int_pi_pi`.`content` CHANGE COLUMN `content_type` `content_type` 
+ENUM('unknown','monograph','serial','other') NULL DEFAULT 'monograph'  ;
 
-
-
-ALTER TABLE `int_pi_pi`.`content` CHANGE COLUMN `content_type` `content_type` ENUM('unknown','book','serial','journal','magazine','other') NULL DEFAULT 'book'  ;
-
+update  int_pi_pi.content set content_type='monograph' where content_type is null or content_type='';
 
