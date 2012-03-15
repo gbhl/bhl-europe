ad328074c2ca4e5fe94f898a96cc1ad6a1a0d1f3
a.kohlbecker <a.kohlbecker@bgbm.org>
2012-03-15 13:07:57 +0100

    minor modification

--- pre-ingest/data-harmonisation/de-uber/fetch-metadata.sh
+++ pre-ingest/data-harmonisation/de-uber/fetch-metadata.sh
@@ -1,15 +1,20 @@
-#!/bin/bash
+#!/bin/bash -e
+
+AA="oc.hu"
+BB="rlin.de"
+CC="${AA}-be${BB}"
+
 
 find -type d -regex "^./[0-9]*$" | egrep -o "[0-9]*" | while read DIR; do
 	echo $DIR
 	cd $DIR
-	curl -o metadata.oai_dc https://edoc.hu-berlin.de/OAI-2.0\?verb\=GetRecord\&identifier\=oai:HUBerlin.de:${DIR}\&metadataPrefix\=oai_dc
-	curl -o metadata.oai_ems https://edoc.hu-berlin.de/OAI-2.0\?verb\=GetRecord\&identifier\=oai:HUBerlin.de:${DIR}\&metadataPrefix\=oai_ems
+	curl -o metadata.oai_dc https://ed${CC}/OAI-2.0\?verb\=GetRecord\&identifier\=oai:HUBerlin.de:${DIR}\&metadataPrefix\=oai_dc
+	curl -o metadata.oai_ems https://ed${CC}/OAI-2.0\?verb\=GetRecord\&identifier\=oai:HUBerlin.de:${DIR}\&metadataPrefix\=oai_ems
 
 	#
 	# get base url
 	#
-	BASE_URL=(`egrep -o "http://edoc.hu-berlin.de/ebind/mfn/[^/]*/XML/" metadata.oai_dc | head -n 1 | sed  "s,/XML/,/,"`)
+	BASE_URL=(`egrep -o "http://ed${CC}/ebind/mfn/[^/]*/XML/" metadata.oai_dc | head -n 1 | sed  "s,/XML/,/,"`)
 	
 	#
 	# download addisional metadata
@@ -22,7 +27,7 @@ find -type d -regex "^./[0-9]*$" | egrep -o "[0-9]*" | while read DIR; do
 	#
 	# download tiffs
 	#
-	wget --no-directories --no-parent -r "${BASE_URL}tif/"
+	#wget --no-directories --no-parent -r "${BASE_URL}tif/"
 	# clean up
 	rm index.*
 	rm robots.txt
