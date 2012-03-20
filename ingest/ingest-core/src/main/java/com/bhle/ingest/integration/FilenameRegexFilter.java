cdc9cc85ff06a2db518a35b1703807b1193e933d
ZhengLIAtos <zheng.li@atosorigin.com>
2012-03-20 16:14:28 +0800

    Add more log entries

--- ingest/ingest-core/src/main/java/com/bhle/ingest/integration/FilenameRegexFilter.java
+++ ingest/ingest-core/src/main/java/com/bhle/ingest/integration/FilenameRegexFilter.java
@@ -3,17 +3,26 @@ package com.bhle.ingest.integration;
 import java.io.File;
 import java.io.FilenameFilter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FilenameRegexFilter implements FilenameFilter {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(FilenameRegexFilter.class);
+
 	private String regex;
-	
+
 	public FilenameRegexFilter(String regex) {
 		this.regex = regex;
 	}
-	
+
 	@Override
 	public boolean accept(File dir, String name) {
-		return name.matches(regex);
+		boolean isValid = name.matches(regex);
+		logger.info("Accept file {} ? {}",
+				new String[] { name, String.valueOf(isValid) });
+		return isValid;
 	}
 
 }
