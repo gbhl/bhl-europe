131fe8ab066251915c4ea7c2ab2196027f4d963f
ZhengLIAtos <zheng.li@atosorigin.com>
2012-03-28 21:54:44 +0800

    MessageHelper.php new version Asynchronous subscriber triggers a callback function.

--- pre-ingest/ingest/stomp-client/receive.php
+++ pre-ingest/ingest/stomp-client/receive.php
@@ -2,10 +2,16 @@
 
 require_once 'MessageHelper.php';
 
+function echo_message($msg) {
+	$body = json_decode($msg->body);
+	var_dump($body);
+	echo "guid: ".$body->GUID."\n";
+	echo "status: ".$body->STATUS."\n";
+	if ($body->STATUS == "FAILED") {
+		echo "exceptions: ".$body->EXCEPTIONS."\n";
+	}
+}
+
 $messageHelper = new MessageHelper("tcp://bhl-mandible.nhm.ac.uk:61613");
-$messageHelper->subscribe();
-$msg = $messageHelper->receive();
-
-echo $msg;
-
+$messageHelper->subscribe('echo_message');
 ?>
