cc5d125de7bb2d88a9d6bb3252de78ff4ff610bc
a.kohlbecker <a.kohlbecker@bgbm.org>
2012-03-29 11:47:34 +0200

    splitting out awk script

--- pre-ingest/data-harmonisation/de-uber/create-structure.sh
+++ pre-ingest/data-harmonisation/de-uber/create-structure.sh
@@ -61,44 +61,7 @@ find -type d -regex "^./[0-9]*$" | egrep -o "[0-9]*" | while read DIR; do
 
 				rm -f ._* # remove all awk generated files
 				
-				awk '{if (index($0, "STRUCTURE")) doPrint=1} 
-					 {if (index($0, "SEQUENCE")) doPrint=0} 
-					 {if ($1 == "ID_Inventar:") {id=$2; print id >> "._id"} }
-					 {if (doPrint==1 && ! match($0,"^--.*") && length($2) > 0 ) { 
-
-					 		n=split($2, pages, "-"); 
-					 		if(n == 2){
-					 			fromPage=pages[1];
-					 			toPage=pages[2];
-					 		} else {
-					 			fromPage=$2;
-					 			toPage=$2;
-					 		}
-
-					 		# one line per chapter subfolder
-					 		i=3;
-					 		chapterLine = "";
-					 		while($i != "") {
-					 			isPageNumber = match($i,"[0-9]+\x2E.*")
-					 			if( lastToken == $i || !isPageNumber ) {
-					 				if(isPageNumber) {
-					 					chapterLine = chapterLine $i;
-						 			} else {
-						 				chapterLine = chapterLine " " $i;
-						 			}
-					 			}	
-					 			lastToken = $i;
-					 			i++;
-					 		}
-					    	print p, chapterLine >> "._chapters";
-					 		
-					 		# one image filename per line
-					 		for(p=fromPage; p<=toPage; p++) {
-					    		print id "_" p  "_" $1 >> "._filenames";
-					 		}
-
-					 	}
-					  }' $METADATAFILE
+				awk -f $WORKDIR/create-structure.awk $METADATAFILE
 
 					 cat  ._filenames
 					 
