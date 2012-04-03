c9ffa9f69b191d9e4bdcd4a0200c46bde2e9c502
ZhengLIAtos <zheng.li@atosorigin.com>
2012-04-03 16:40:35 +0800

    Sort ingest files (place book object at the bottom)

--- ingest/ingest-core/src/test/java/com/bhle/ingest/BatchIngestTest.java
+++ ingest/ingest-core/src/test/java/com/bhle/ingest/BatchIngestTest.java
@@ -49,9 +49,9 @@ public class BatchIngestTest implements ResourceLoaderAware {
 		Map<String, String> messageBody = new HashMap<String, String>();
 		logger.info("Create Test Message...");
 		
-		messageBody.put(MSG_GUID_NAME, "10706-a0000000000000000000test");
+		messageBody.put(MSG_GUID_NAME, "bhle:10706/a000test");
 		logger.info("MessageBody: {}:{}", MSG_GUID_NAME,
-				"10706-a0000000000000000000test");
+				"bhle:10706/a000test");
 
 		Resource aip = resourceLoader
 				.getResource("classpath:/com/bhle/ingest/.aip");
@@ -65,7 +65,13 @@ public class BatchIngestTest implements ResourceLoaderAware {
 
 	@Test
 	public void testExistenceOfSampleByPurge() throws InterruptedException {
-		int statusCode = service.purge("bhle:10706-a0000000000000000000test");
+		int statusCode = service.purge("bhle:10706-a000test");
+		Assert.assertEquals(200, statusCode);
+		
+		statusCode = service.purge("bhle:10706-a000test-00001");
+		Assert.assertEquals(200, statusCode);
+		
+		statusCode = service.purge("bhle:10706-a000test-00002");
 		Assert.assertEquals(200, statusCode);
 		
 		Thread.sleep(5000);
