98aec4b19d20ab234afb98c3e1f371a8d9f32efc
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2012-08-23 10:51:08 +0200

    fixing typo in queueing functions

--- pre-ingest/ingest/inc/ingest_tools.php
+++ pre-ingest/ingest/inc/ingest_tools.php
@@ -143,7 +143,7 @@ function queue_add($queueFile, $arrQueueCommands)
             }
         }*/
         // Write all commands straight ahead
-        $content2write = implode('\n', $arrQueueCommands);
+        $content2write = implode("\n", $arrQueueCommands);
 
         if ($content2write!="")
         $nBytes = file_put_contents($queueFile, "\n".$content2write."\n", FILE_APPEND);
