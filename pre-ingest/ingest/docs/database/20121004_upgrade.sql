7483c6d58b3e67dd80a740326ab12a6cf2bd8462
viras <viras@users.sourceforge.net>
2012-10-04 15:34:59 +0200

    waiting status added

--- pre-ingest/ingest/docs/database/20121004_upgrade.sql
+++ pre-ingest/ingest/docs/database/20121004_upgrade.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `ingest_queue`  ADD `item_count` INT NOT NULL DEFAULT '0',  ADD `items_done` INT NOT NULL DEFAULT '0';
+
+ALTER TABLE `ingest_queue` CHANGE `ingest_status` `ingest_status` ENUM('queued','waiting','running','finished','error') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'queued';
