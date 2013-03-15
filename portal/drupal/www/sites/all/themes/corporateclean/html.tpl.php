2466f08bd0b0a2c756de5ec5387ca6871c382240
wkollernhm <wolfgang.koller@nhm-wien.ac.at>
2013-03-15 12:05:26 +0100

    disabling piwik & enabling google analytics

--- portal/drupal/www/sites/all/themes/corporateclean/html.tpl.php
+++ portal/drupal/www/sites/all/themes/corporateclean/html.tpl.php
@@ -18,5 +18,22 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+
+<!-- google analytics code -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20529678-2']);
+  _gaq.push(['_setDomainName', 'bhl-europe.eu']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </body>
 </html>
